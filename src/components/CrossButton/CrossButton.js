d553c1a6e4a6197da6a3b802d3920395788bc3ab
Deepak Gupta <deepak.gupta07@nagarro.com>
2019-08-05 16:21:07 +0530

    Changed directory name to lowerCase

--- src/components/CrossButton/CrossButton.js
+++ src/components/CrossButton/CrossButton.js
@@ -1,33 +1,31 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from "./node_modules/react";
+import PropTypes from "./node_modules/prop-types";
 
 import "./crossbutton.css";
 
 /**
  * @description Cross button renders X if the props.value is not null
  */
-const CrossButton = (props) => {
+const CrossButton = props => {
   const { value, onChangeInput, name } = props;
   return (
-
     <div
       className={`input-group-addon ${value && "visible"} `}
       onClick={() => onChangeInput("", name)}
     >
       X
     </div>
-
   );
 };
 
 CrossButton.defaultProps = {
-  value: "default",
+  value: "default"
 };
 
 CrossButton.propTypes = {
   value: PropTypes.bool,
   name: PropTypes.string.isRequired,
-  onChangeInput: PropTypes.func.isRequired,
+  onChangeInput: PropTypes.func.isRequired
 };
 
 export default CrossButton;
--- src/container/input/inputControl.js
+++ src/container/input/inputControl.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-import CrossButton from "../../components/CrossButton/crossButton";
+import CrossButton from "../../components/crossbutton/CrossButton";
 import ButtonControl from "../../components/button/button";
 
 import {
